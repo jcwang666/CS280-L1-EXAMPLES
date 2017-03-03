71dba736348c3faf4a4575f982ac7037115ad0bc
jcwang666 <j2204620@gmail.com>
2017-03-03 22:19:53 +0800

    pokeman

--- CS280-L1-EXAMPLES/Program.cs
+++ CS280-L1-EXAMPLES/Program.cs
@@ -10,9 +10,17 @@ namespace CS280_L1_EXAMPLES
     {
         private static void Main(string[] args)
         {
+            Pokemon pokemon = new Pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+
             Console.WriteLine("HELLO WORLD");
             Console.WriteLine("HELLO WORLD2222");
             Console.WriteLine("HELLO WORLD23333");
+            Console.WriteLine("height:{0} , weight:{1}", pokemon.Height, pokemon.Weight);
         }
     }
 }
